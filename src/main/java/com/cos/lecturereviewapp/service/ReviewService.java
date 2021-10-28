54fac514d2c976a5fcdb666213f21bd26f9e091a
ssar <ssar@example.com>
2021-10-28 17:37:17 +0900

    [박민영] 리뷰수정, 등록 페이지v1

--- src/main/java/com/cos/lecturereviewapp/service/ReviewService.java
+++ src/main/java/com/cos/lecturereviewapp/service/ReviewService.java
@@ -1,9 +1,15 @@
 package com.cos.lecturereviewapp.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.lecturereviewapp.domain.Board.Board;
 import com.cos.lecturereviewapp.domain.Board.BoardRepository;
+import com.cos.lecturereviewapp.domain.review.Review;
 import com.cos.lecturereviewapp.domain.review.ReviewRepository;
+import com.cos.lecturereviewapp.domain.user.User;
+import com.cos.lecturereviewapp.handler.ex.MyNotFoundException;
+import com.cos.lecturereviewapp.web.dto.ReviewSaveReqDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,5 +18,21 @@ import lombok.RequiredArgsConstructor;
 public class ReviewService {
 	private final BoardRepository boardRepository;
 	private final ReviewRepository reviewRepository;
+	
+	@Transactional(rollbackFor = MyNotFoundException.class)
+	public void reviewReg(int boardId, ReviewSaveReqDto dto, User principal) {
+		
+		Board boardEntity = boardRepository.findById(boardId)
+				.orElseThrow(() -> new MyNotFoundException("해당 게시글을 찾을 수 없습니다."));
+		
+		Review review = new Review();
+		review.setTitle(dto.getTitle());
+		review.setContent(dto.getContent());
+		review.setRating(dto.getRating());
+		review.setUser(principal);
+		review.setBoard(boardEntity);
+		
+		reviewRepository.save(review);
+	}
 
 }
--- src/main/java/com/cos/lecturereviewapp/web/BoardController.java
+++ src/main/java/com/cos/lecturereviewapp/web/BoardController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import com.cos.lecturereviewapp.domain.user.User;
 import com.cos.lecturereviewapp.service.BoardService;
 import com.cos.lecturereviewapp.service.ReviewService;
 import com.cos.lecturereviewapp.web.dto.CMRespDto;
+import com.cos.lecturereviewapp.web.dto.ReviewSaveReqDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -34,11 +36,23 @@ public class BoardController {
 	// 주완 - 강의 등록 페이지 이동 @GetMapping("/board/saveForm")
 	
 	//민영 - 리뷰 수정 @PutMapping("/board/{id}/review")
-
+	@PutMapping("/board/{id}/review")
+	public CMRespDto<String> reviewUpdate() {
+		return new CMRespDto<>(1, "업데이트 성공", null);
+	}
 	//민영 - 리뷰 수정페이지 이동 @GetMapping("/board/reviewupdateForm")
-
+	@GetMapping("/board/{boardId}/reviewupdateForm")
+	public String reviewupdateForm() {
+		return "board/reviewupdateForm";
+	}
 	//민영 - 리뷰 등록 @PostMapping("/board/{boardId}/review")
-
+	@PostMapping("/board/{boardId}/review")
+	public String review(int boardId, ReviewSaveReqDto dto) {
+		User principal = (User) session.getAttribute("principal");
+		
+		reviewService.reviewReg(boardId, dto, principal);
+		return "redirect:/board/"+boardId;
+	}
 	//민영 - 리뷰 쓰기페이지 이동 @GetMapping
 	@GetMapping("/board/{boardId}/reviewsaveForm")
 	public String reviewsaveForm() {
